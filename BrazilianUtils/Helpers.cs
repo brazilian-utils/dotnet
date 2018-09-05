e26e0b4d6302d2ac1bc6a5af78d55485f27ef61b
Rodinei <rodinei.jf@gmail.com>
2018-09-04 21:52:15 -0300

    Update OnlyNumber for better performance

--- BrazilianUtils/Helpers.cs
+++ BrazilianUtils/Helpers.cs
@@ -1,9 +1,20 @@
-using System.Text.RegularExpressions;
+using System.Text;
 
 namespace BrazilianUtils
 {
     public static class Helpers
     {
-        public static string OnlyNumbers(string input) => Regex.Replace(input, @"[^\d]", string.Empty);
+        public static string OnlyNumbers(string input)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var c in input)
+            {
+                if (char.IsDigit(c))
+                    sb.Append(c);
+            }
+
+            return sb.ToString();
+        }
     }
 }
