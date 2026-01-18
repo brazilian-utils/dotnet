83ba40635e2ff8705f10c02f5803d8718d18170b
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2026-01-17 16:56:33 -0800

    Adição de novas regras para CNPJ. (#10)

--- BrazilianUtils.Tests/CnpjTests.fs
+++ BrazilianUtils.Tests/CnpjTests.fs
@@ -8,6 +8,9 @@ open Xunit
 [<InlineData"46.238.497/0001-81">]
 [<InlineData"18240603000126">]
 [<InlineData"77973689000165">]
+// Alphanumeric CNPJs (Instrução Normativa RFB nº 2.119)
+[<InlineData"12.ABC.345/01DE-35">]
+[<InlineData"12ABC34501DE35">]
 let shouldBeValid cnpj =
     cnpj
     |> Cnpj.IsValid
@@ -18,10 +21,12 @@ let shouldBeValid cnpj =
 [<InlineData null>]
 [<InlineData"123456">]
 [<InlineData"11257245286">]
-[<InlineData"abcabcabcde">]
 [<InlineData"11111111111">]
 [<InlineData"77973689000163">]
 [<InlineData"77173389000163">]
+// Invalid alphanumeric CNPJs
+[<InlineData"12.ABC.345/01DE-99">]
+[<InlineData"AAAAAAAAAAAA00">]
 let shouldBeInvalid cnpj =
     cnpj
     |> Cnpj.IsValid
@@ -30,11 +35,23 @@ let shouldBeInvalid cnpj =
 [<Theory>]
 [<InlineData"22938962000129">]
 [<InlineData"42.999.072/0001-34">]
-let shouldFormattedStringMatchRegex cnpj =
+let shouldFormattedNumericStringMatchRegex cnpj =
     Assert.Matches(@"^[0-9]{2}(\.[0-9]{3}){2}\/[0-9]{4}\-[0-9]{2}$", Cnpj.Format cnpj)
+
+[<Theory>]
+[<InlineData"12ABC34501DE35">]
+[<InlineData"12.ABC.345/01DE-35">]
+let shouldFormattedAlphanumericStringMatchRegex cnpj =
+    Assert.Matches(@"^[0-9A-Z]{2}(\.[0-9A-Z]{3}){2}\/[0-9A-Z]{4}\-[0-9]{2}$", Cnpj.Format cnpj)
 
 [<Fact>]
 let generatedCnpjShouldBeValid() =
     [ 1..100 ]
     |> Seq.forall (fun _ -> Cnpj.Generate() |> Cnpj.IsValid)
+    |> Assert.True
+
+[<Fact>]
+let generatedAlphanumericCnpjShouldBeValid() =
+    [ 1..100 ]
+    |> Seq.forall (fun _ -> Cnpj.GenerateAlphanumeric() |> Cnpj.IsValid)
     |> Assert.True
